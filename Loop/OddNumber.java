5a486b21f7f602f7a757afe635f365920efe4da0
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-05 09:06:09 +0530

    .

---
+++ Loop/OddNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class OddNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Odd numbers from 1 to " + n + " are:");
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+    
+}
