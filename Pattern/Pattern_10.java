f98f2e4f0365fd438d57d58a312cb7748e057a04
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-07 23:21:37 +0530

    .

---
+++ Pattern/Pattern_10.java
@@ -0,0 +1,23 @@
+    // 1 1 1 1 
+    //  2 2 2 
+    //   3 3 
+    //    4 
+public class Pattern_10 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<n ;i++){
+        for(int j=0; j<i;j++){
+            System.out.print(" ");
+        }
+        for(int j=0; j<n-i;j++){
+            System.out.print(i+1+" ");
+        }
+        System.out.println();
+    }
+    
+
+    }
+
+    }
+    
+
