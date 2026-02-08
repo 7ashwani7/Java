2e0bb32eaf0ed1515c7f1a1f450bff60c4120843
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-08 17:03:37 +0530

    .

---
+++ Pattern/Pattern_14.java
@@ -0,0 +1,21 @@
+// Hollow Square Pattern
+    // * * * * *
+    // *       *
+    // *       *
+    // * * * * *
+public class Pattern_14 {
+    public static void main(String[] args) {
+        int n = 4; // Size of the square
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
