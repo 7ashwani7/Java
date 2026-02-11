fce1f91d98a1f34ef9835ac8e738ddd7be4dc06c
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-11 09:02:11 +0530

    .

---
+++ 6-Array/Max_Min.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Max_Min {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        System.out.println("Maximum number: " + max);
+        System.out.println("Minimum number: " + min);
+    }
+}
