1a18321c915c3f25b2722c8652ec1fb67142ae43
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-12 18:09:55 +0530

    .

---
+++ 7-2DArray/Transpose.java
@@ -0,0 +1,29 @@
+// For a given matrix of N x M, print its transpose.
+import java.util.*;
+public class Transpose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the elements of the matrix: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("The transpose of the matrix is: ");
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < rows; i++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
