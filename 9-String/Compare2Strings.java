595d667b255545f8a882b0c7c51658d86e95ec54
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-15 18:54:47 +0530

    .

---
+++ 9-String/Compare2Strings.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Compare2Strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first string:");
+        String str1 = sc.nextLine();
+        System.out.print("Enter the second string:");
+        String str2 = sc.nextLine();
+
+        if (str1.equals(str2)) {
+            System.out.println("The strings are equal.");
+        } else {
+            System.out.println("The strings are not equal.");
+        }
+         // Do not use 
+        // if(str1 == str2){
+        //     System.out.println("The strings are equal.");
+        // } else {
+        //     System.out.println("The strings are not equal.");
+        // }
+    }
+    
+}
