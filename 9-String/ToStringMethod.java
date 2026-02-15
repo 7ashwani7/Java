2b75187d87684466aba370cdec9085687b09d202
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-15 19:00:29 +0530

    .

---
+++ 9-String/ToStringMethod.java
@@ -0,0 +1,9 @@
+public class ToStringMethod {
+    public static void main(String[] args) {
+        int num = 123;
+        String strNum = Integer.toString(num);
+        
+        System.out.println("The integer number is: " + num);
+        System.out.println("The string representation of the number is: " + strNum);
+    }
+}
