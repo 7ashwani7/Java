a6d249aba888d9ec01e26944a2f1db4084e914f8
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-15 18:57:07 +0530

    .

---
+++ 9-String/ParseIntMethod.java
@@ -0,0 +1,10 @@
+public class ParseIntMethod {
+    public static void main(String[] args) {
+        String strNum = "12345";
+        int num = Integer.parseInt(strNum);
+        
+        System.out.println("The string number is: " + strNum);
+        System.out.println("The integer number is: " + num);
+    }
+    
+}
