f81fa84210a0b385dfa7e079b2b1bb3be3a84376
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-16 12:49:43 +0530

    .

---
+++ 10-StringBuilder/GetCharIndex.java
@@ -0,0 +1,11 @@
+public class GetCharIndex {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();// StringBuilder("Hello"); 
+        sb.append("Hello");
+        System.out.println(sb.charAt(0));
+        System.out.println(sb.charAt(1));
+        System.out.println(sb.charAt(2));
+        System.out.println(sb.charAt(3));
+        System.out.println(sb.charAt(4));
+    }
+}
