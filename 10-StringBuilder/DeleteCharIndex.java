397c0abad98afdebc643bfa7326c45cf21519470
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-17 12:32:24 +0530

    .

---
+++ 10-StringBuilder/DeleteCharIndex.java
@@ -0,0 +1,7 @@
+public class DeleteCharIndex {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello World");
+        sb.deleteCharAt(5);
+        System.out.println(sb);
+    }
+}
