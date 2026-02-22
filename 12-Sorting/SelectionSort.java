5664be26e1a3c089fe097138d2660ce1c0971c9d
7ashwani7 <chauhanashwani548@gmail.com>
2026-02-22 17:15:08 +0530

    .

---
+++ 12-Sorting/SelectionSort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        selectionSort(arr);
+        System.out.print("Sorted array:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+    
+}
