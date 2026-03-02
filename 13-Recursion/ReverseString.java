f2c49617f4cebca7cce3bf1aa580db7f5f27c9b1
7ashwani7 <chauhanashwani548@gmail.com>
2026-03-02 19:56:25 +0530

    .

---
+++ 13-Recursion/ReverseString.java
@@ -0,0 +1,15 @@
+public class ReverseString {
+    public static String reverse(String str) {
+        if (str.length() == 1) {
+            return str;
+        }
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+        public static void main(String[] args) {
+        String str = "Hello, World!";
+        String reversedStr = reverse(str);
+        System.out.println("Original string: " + str);
+        System.out.println("Reversed string: " + reversedStr);
+    }
+    
+}
