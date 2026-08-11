9e73928e77d78bf03f93651813934df6ca8c4aa2
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-11 22:31:30 +0530

    .

---
+++ 01-Basic/Introduction.java
@@ -0,0 +1,8 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("I am learning Java.");
+        System.out.println("I know C++.");
+    }
+}
